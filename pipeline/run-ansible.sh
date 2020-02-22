9b379718f07b23aff11f6139e4e011afcf10fdb9
Venkata Sai Madduri <vpmaddur@ncsu.edu>
2020-02-22 01:48:20 +0000

    Modified to accept Mongo username/password from user

--- pipeline/run-ansible.sh
+++ pipeline/run-ansible.sh
@@ -13,12 +13,14 @@ function die {
 }
 
 # Check the number of arguments
-[ $# -ge 5 ] || die "usage: $0 <playbook> <inventory>"
+[ $# -ge 7 ] || die "usage: $0 <playbook> <inventory>"
 
 PLAYBOOK=$1
 INVENTORY=$2
 JK_USERNAME=$3
 JK_PASSWORD=$4
 VAULT_PASSWORD_FILE=$5
+MO_USERNAME=$6
+MO_PASSWORD=$7
 
-ansible-playbook --vault-password-file $VAULT_PASSWORD_FILE $PLAYBOOK -i $INVENTORY -e "{'JK_USR': $JK_USERNAME, 'JK_PASSWD': $JK_PASSWORD}"
+ansible-playbook --vault-password-file $VAULT_PASSWORD_FILE $PLAYBOOK -i $INVENTORY -e "{'JK_USR': $JK_USERNAME, 'JK_PASSWD': $JK_PASSWORD, 'MO_USR': $MO_USERNAME, 'MO_PASSWD': $MO_PASSWORD}"
