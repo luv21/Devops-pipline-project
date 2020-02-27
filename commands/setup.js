6c4bb339742aec01eff7a82c2fdf712aeceb6bf2
sjbondu <sjbondu@ncsu.edu>
2020-02-27 09:50:34 +0000

    builder

--- commands/setup.js
+++ commands/setup.js
@@ -47,20 +47,20 @@ exports.handler = async argv => {
 
 async function run(privateKey) {
 
-    console.log(chalk.greenBright('Installing configuration server!'));
+    // console.log(chalk.greenBright('Installing configuration server!'));
 
-    console.log(chalk.blueBright('Provisioning configuration server...'));
-    let result = child.spawnSync(`bakerx`, `run ansible-srv bionic --ip 192.168.33.10 --sync`.split(' '), {shell:true, stdio: 'inherit'} );
-    if( result.error ) { console.log(result.error); process.exit( result.status ); }
+    // console.log(chalk.blueBright('Provisioning configuration server...'));
+    // let result = child.spawnSync(`bakerx`, `run ansible-srv bionic --ip 192.168.33.10 --sync`.split(' '), {shell:true, stdio: 'inherit'} );
+    // if( result.error ) { console.log(result.error); process.exit( result.status ); }
 
-    console.log(chalk.blueBright('Provisioning jenkins server...'));
-    result = child.spawnSync(`bakerx`, `run jenkins-srv bionic --ip 192.168.33.20`.split(' '), {shell:true, stdio: 'inherit'} );
-    if( result.error ) { console.log(result.error); process.exit( result.status ); }
+    // console.log(chalk.blueBright('Provisioning jenkins server...'));
+    // result = child.spawnSync(`bakerx`, `run jenkins-srv bionic --ip 192.168.33.20`.split(' '), {shell:true, stdio: 'inherit'} );
+    // if( result.error ) { console.log(result.error); process.exit( result.status ); }
 
-    console.log(chalk.blueBright('Installing privateKey on configuration server'));
-    let identifyFile = privateKey || path.join(os.homedir(), '.bakerx', 'insecure_private_key');
-    result = scpSync (identifyFile, 'vagrant@192.168.33.10:/home/vagrant/.ssh/jk_rsa');
-    if( result.error ) { console.log(result.error); process.exit( result.status ); }
+    // console.log(chalk.blueBright('Installing privateKey on configuration server'));
+    // let identifyFile = privateKey || path.join(os.homedir(), '.bakerx', 'insecure_private_key');
+    // result = scpSync (identifyFile, 'vagrant@192.168.33.10:/home/vagrant/.ssh/jk_rsa');
+    // if( result.error ) { console.log(result.error); process.exit( result.status ); }
 
     console.log(chalk.blueBright('Running init script...'));
     result = sshSync('/bakerx/pipeline/server-init.sh', 'vagrant@192.168.33.10');
