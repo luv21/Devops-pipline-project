e736eb010ed242de22fe5aa8f349b06ad52bcaac
Venkata Sai Madduri <vpmaddur@ncsu.edu>
2020-02-28 00:20:44 +0000

    Global vars declared

--- commands/setup.js
+++ commands/setup.js
@@ -34,6 +34,7 @@ exports.handler = async argv => {
 
         await run( privateKey );
         if (fs.existsSync(path.resolve('pipeline/playbook.yml')) && fs.existsSync(path.resolve('pipeline/inventory.ini'))) {
+	    console.log("hi");
             await jenkins_setup('pipeline/playbook.yml', 'pipeline/inventory.ini', vaultfilePath);
         }
 
@@ -47,20 +48,21 @@ exports.handler = async argv => {
 
 async function run(privateKey) {
 
-    // console.log(chalk.greenBright('Installing configuration server!'));
+    console.log(chalk.greenBright('Installing configuration server!'));
 
-    // console.log(chalk.blueBright('Provisioning configuration server...'));
-    // let result = child.spawnSync(`bakerx`, `run ansible-srv bionic --ip 192.168.33.10 --sync`.split(' '), {shell:true, stdio: 'inherit'} );
-    // if( result.error ) { console.log(result.error); process.exit( result.status ); }
+    console.log(chalk.blueBright('Provisioning configuration server...'));
+    let result = child.spawnSync(`bakerx`, `run ansible-srv bionic --ip 192.168.33.10 --sync`.split(' '), {shell:true, stdio: 'inherit'} );
+    if( result.error ) { console.log(result.error); process.exit( result.status ); }
 
-    // console.log(chalk.blueBright('Provisioning jenkins server...'));
-    // result = child.spawnSync(`bakerx`, `run jenkins-srv bionic --ip 192.168.33.20`.split(' '), {shell:true, stdio: 'inherit'} );
-    // if( result.error ) { console.log(result.error); process.exit( result.status ); }
+    console.log(chalk.blueBright('Provisioning jenkins server...'));
+    result = child.spawnSync(`bakerx`, `run jenkins-srv bionic --ip 192.168.33.20`.split(' '), {shell:true, stdio: 'inherit'} );
+    if( result.error ) { console.log(result.error); process.exit( result.status ); }
 
-    // console.log(chalk.blueBright('Installing privateKey on configuration server'));
-    // let identifyFile = privateKey || path.join(os.homedir(), '.bakerx', 'insecure_private_key');
-    // result = scpSync (identifyFile, 'vagrant@192.168.33.10:/home/vagrant/.ssh/jk_rsa');
-    // if( result.error ) { console.log(result.error); process.exit( result.status ); }
+    console.log(chalk.blueBright('Installing privateKey on configuration server'));
+    let identifyFile = privateKey || path.join(os.homedir(), '.bakerx', 'insecure_private_key');
+    result = scpSync (identifyFile, 'vagrant@192.168.33.10:/home/vagrant/.ssh/jk_rsa');
+    console.log(result)
+    if( result.error ) { console.log(result.error); process.exit( result.status ); }
 
     console.log(chalk.blueBright('Running init script...'));
     result = sshSync('/bakerx/pipeline/server-init.sh', 'vagrant@192.168.33.10');
